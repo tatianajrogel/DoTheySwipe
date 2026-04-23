e9b066d5cbaf42993334283c4c6af1f3491b4af3
tatianajrogel <tatianajrogel@users.noreply.github.com>
2026-04-23 11:00:00 -0500

    feat: integrate ChakraProvider and theme system
    - Wrap app with ChakraProvider using custom theme system
    - Remove Tailwind CSS imports
    - Add index.css placeholder for Chakra-managed styles
    
    Co-authored-by: skip

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { system } from "./theme";
 import App from "./App.jsx";
-import './App.css';
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider value={system}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
